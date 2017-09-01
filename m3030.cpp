a4a277166c100d6790a7e11644bea1057063429f
dabarono <dabarono@unal.edu.co>
2017-09-01 10:48:36 -0500

    por fin funciona

--- m3030.cpp
+++ m3030.cpp
@@ -1,28 +1,54 @@
 #include <iostream>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Core>
-//#include <eigen3/Eigen/Eigenvalues>
+#include <eigen3/Eigen/Eigenvalues>
 using Eigen::MatrixXd;
+using Eigen::SelfAdjointEigenSolver;
 
-int h(int i, int j){
+int h(int i, int j, int n){
   int f;
-  if (j<=i){
-    f=3-i;
+  if (i<=j){
+    f=n-j;
   }
   else{
-    f=0;
+    f=n-i;
   }
   return f;
 }
 
+double vp(int i,int n){
+  double f=1/(2-2*cos((2*i-1)*M_PI/(2*n+1)));
+  return f;
+}
+
+double er(double experimental, double teorico){
+  double f=fabs(experimental-teorico)/teorico;
+  return f;
+}
+
 int main(void){
-  MatrixXd m(3,3);
-  int i=0;
-  int j=0;
-  for (i;i<=2;i=i+1){
-    for (j;j<=2;j=j+1){
-      m(i,j)=h(i,j);
+  std::cout.precision(4);
+  int n=30;
+  MatrixXd m(n,n);
+  for (int i=0;i<=n-1;i=i+1){
+    for (int j=0;j<=n-1;j=j+1){
+      m(i,j)=h(i,j,n);
     }
   }
+  //Para graficar la matriz
+  std::cout<<"Matriz"<<std::endl;
   std::cout<<m<<std::endl;
+  std::cout.setf(std::ios::scientific);
+  SelfAdjointEigenSolver<MatrixXd> es(m);
+  //Para sacar los eigen valores
+  std::cout<<"i"<<"\t"<<"Auto_valores"<<"\t"<<"Auto_valores_t"<<"\t"<<"Error_porcentual"<<std::endl;
+  for(int k=1;k<=n;k=k+1){
+    double u=es.eigenvalues()[n-k];
+    std::cout<<k<<"\t"<<u<<"\t"<<vp(k,n)<<"\t"<<er(u,vp(k,n))<<std::endl;
+  }
+  //Para sacar los eigen vectores
+  std::cout<<"Matriz_de_auto_vectores"<<std::endl;
+  std::cout.unsetf(std::ios::scientific);
+  MatrixXd w=es.eigenvectors();
+  std::cout<<w<<std::endl;
 }
